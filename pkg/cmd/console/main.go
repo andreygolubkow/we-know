373f2756d784205225f8534bb27d3060ed57968f
andreygolubkow <andreygolubkow@gmail.com>
2024-01-18 03:09:59 +0400

    Extract methods

--- pkg/cmd/console/main.go
+++ pkg/cmd/console/main.go
@@ -10,71 +10,87 @@ import (
 	"os"
 )
 
-// main.exe <giturl> <branch>
+const (
+	RefFormat      = "refs/heads/%s"
+	NothingToClone = "Nothing to clone"
+)
+
 func main() {
-
 	if len(os.Args) != 3 {
-		log.Fatal("Nothing to clone")
+		log.Fatal(NothingToClone)
+		return
+	}
+	repositoryLink := os.Args[1]
+	branch := os.Args[2]
+	workingDir, _ := os.Getwd()
+	tmpDir := workingDir + "/tmp"
+
+	if err := os.MkdirAll(tmpDir, 0755); err != nil {
 		return
 	}
 
-	var repositoryLink string = os.Args[1]
-	var branchName string = os.Args[2]
-	var workingDir, _ = os.Getwd()
-	var tmpDir = workingDir + "/tmp"
-	var dirErr = os.MkdirAll(tmpDir, 0755)
-	if dirErr != nil {
-		return
+	repo, err := setUpRepository(tmpDir, repositoryLink, branch)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	var repo *git.Repository
-	repo, err := git.PlainOpen(tmpDir)
+	refName, err := checkoutBranch(repo, branch)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print(refName)
+}
+
+func setUpRepository(tmpDir, repositoryLink, branch string) (rep *git.Repository, err error) {
+	rep, err = git.PlainOpen(tmpDir)
+
 	if errors.Is(err, git.ErrRepositoryNotExists) {
-		repo, err = git.PlainClone(tmpDir, false, &git.CloneOptions{
+		rep, err = git.PlainClone(tmpDir, false, &git.CloneOptions{
 			URL:           repositoryLink,
 			Progress:      os.Stdout,
-			ReferenceName: plumbing.ReferenceName("refs/heads/" + branchName),
+			ReferenceName: plumbing.ReferenceName(fmt.Sprintf(RefFormat, branch)),
 		})
+	}
+
+	return
+}
+
+func checkoutBranch(repo *git.Repository, branch string) (branchName string, err error) {
+	ref, err := repo.Head()
+	if err != nil {
+		return
+	}
+
+	branchName = ref.Name().Short()
+	if branchName != branch {
+		err = fetchAndCheckout(repo, branch)
 		if err != nil {
-			log.Fatal(err)
 			return
 		}
 	}
 
-	ref, err := repo.Head()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+	return
+}
 
-	log.Print(ref.Name())
-
-	// Fetch remote branches
-	fetchErr := repo.Fetch(&git.FetchOptions{
+func fetchAndCheckout(repo *git.Repository, branch string) error {
+	err := repo.Fetch(&git.FetchOptions{
 		RemoteName: "origin",
 		RefSpecs:   []config.RefSpec{"+refs/heads/*:refs/heads/*"},
 	})
-	if fetchErr != nil && fetchErr != git.NoErrAlreadyUpToDate {
-		fmt.Println(fetchErr)
-		os.Exit(1)
+
+	if err != nil && !errors.Is(err, git.NoErrAlreadyUpToDate) {
+		return err
 	}
 
-	// Get the working directory for the repository
-	w, err := repo.Worktree()
+	workTree, err := repo.Worktree()
 	if err != nil {
-		log.Fatal(err)
-		return
+		return err
 	}
 
-	err = w.Checkout(&git.CheckoutOptions{
-		Branch: plumbing.ReferenceName("refs/heads/master"),
+	err = workTree.Checkout(&git.CheckoutOptions{
+		Branch: plumbing.ReferenceName(fmt.Sprintf(RefFormat, branch)),
 	})
 
-	ref, err = repo.Head()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	log.Print(ref.Name())
+	return err
 }
